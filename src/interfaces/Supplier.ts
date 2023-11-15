55beee4e576b477b9ce1984ce0e988bb084b10fe
DucNguyen <nguyenngocduc94751@gmail.com>
2023-11-15 17:39:46 +0700

    create Customer, Employee, Order page. fixed variant for product

---
+++ src/interfaces/Supplier.ts
@@ -0,0 +1,12 @@
+export interface ISupplier {
+  _id?: object;
+  name?: string;
+  img_url?: string;
+  email?: string;
+  phone_number?: string;
+  address?: string;
+  affiliated_website?: string;
+  is_delete?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
---
+++ src/interfaces/Variant.ts
@@ -0,0 +1,9 @@
+export interface Variant {
+  _id: object;
+  title: string;
+  price: number;
+  stock: number;
+  position: number;
+  variant_image: string;
+  product_id: object;
+}
