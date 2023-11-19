fb795942c78902286145735298361fb74227b2ac
nguyenducquyen <nguyenducquyen2k1@gmail.com>
2023-11-19 10:53:47 +0700

    push customers employee

---
+++ src/interfaces/Customer.ts
@@ -0,0 +1,12 @@
+export interface ICustomer {
+  _id?: object;
+  full_name?: string;
+  image_url?: string;
+  email?: string;
+  phone_number?: string;
+  address?: string;
+  active?: boolean;
+  is_delete?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
---
+++ src/interfaces/Employees.ts
@@ -0,0 +1,13 @@
+export interface IEmployees {
+  _id?: object;
+  full_name?: string;
+  image_url?: string;
+  email?: string;
+  phone_number?: string;
+  address?: string;
+  active?: boolean;
+  roles?: string;
+  is_delete?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
